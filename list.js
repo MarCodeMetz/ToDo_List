e1f37cb8e14c137cf0b123c88474cfc9597d723f
Marco Metz <marco.metz@yahoo.de>
2019-09-04 17:30:49 -0500

    first commit

--- list.js
+++ list.js
@@ -1,7 +1,18 @@
-var toDos = [];
+var toDos = ["Buy new Turtle"];
 
 var input = prompt("What would you like to do?");
 
-if(input === "list") {
-    console.log(toDos);
+while (input !== "QUIT") {
+    //handle input
+    if (input === "LIST") {
+        console.log(toDos);
+    } else if (input === "NEW") {
+        //ask for new todo
+        var newTodo = prompt("Enter new todo");
+        //add to todo-list a new array
+        toDos.push(newTodo);
+    }
+    //ask again for new input
+    input = prompt("What would you like to do?");
 }
+console.log("OK, YOU QUIT THE APP!");
